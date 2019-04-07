2d694ec09715995e0eecd1d05b041c7a4ca14dc6
swar <swar.m.patel@gmail.com>
2019-04-07 05:32:40 -0400

    Reverting headers

--- nba_api/stats/library/http.py
+++ nba_api/stats/library/http.py
@@ -7,13 +7,11 @@ try:
 except ImportError:
     STATS_HEADERS = {
         'Host': 'stats.nba.com',
-        'Connection': 'keep-alive',
-        'Cache-Control': 'max-age=0',
-        'Upgrade-Insecure-Requests': '1',
-        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36',
-        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3',
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:61.0) Gecko/20100101 Firefox/61.0',
+        'Accept': 'application/json, text/plain, */*',
+        'Accept-Language': 'en-US,en;q=0.5',
         'Accept-Encoding': 'gzip, deflate, br',
-        'Accept-Language': 'en-US,en;q=0.9',
+        'Connection': 'keep-alive',
     }
 
 
