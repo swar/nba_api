a9b25a45796999664405ba22ab90a6788ef625a5
swar <swar.m.patel@gmail.com>
2019-04-07 04:35:58 -0400

    Updating headers

--- nba_api/stats/library/http.py
+++ nba_api/stats/library/http.py
@@ -7,11 +7,13 @@ try:
 except ImportError:
     STATS_HEADERS = {
         'Host': 'stats.nba.com',
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:61.0) Gecko/20100101 Firefox/61.0',
-        'Accept': 'application/json, text/plain, */*',
-        'Accept-Language': 'en-US,en;q=0.5',
-        'Accept-Encoding': 'gzip, deflate, br',
         'Connection': 'keep-alive',
+        'Cache-Control': 'max-age=0',
+        'Upgrade-Insecure-Requests': '1',
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36',
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3',
+        'Accept-Encoding': 'gzip, deflate, br',
+        'Accept-Language': 'en-US,en;q=0.9',
     }
 
 
@@ -92,7 +94,7 @@ class NBAStatsHTTP(http.NBAHTTP):
 
     nba_response = NBAStatsResponse
 
-    base_url = 'http://stats.nba.com/stats/{endpoint}'
+    base_url = 'https://stats.nba.com/stats/{endpoint}'
 
     headers = STATS_HEADERS
 
