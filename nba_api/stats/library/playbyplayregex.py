85ec8137e0bf5a86a99c76f9d34b1c804a3691b1
Randy Forbes <randy.forbes@gmail.com>
2019-04-27 23:47:56 -0400

    updated play by play tests, test data, and added new integration test to validate previous days play by play data.

--- nba_api/stats/library/playbyplayregex.py
+++ nba_api/stats/library/playbyplayregex.py
@@ -1,25 +1,29 @@
 import re
+from collections import defaultdict
+from nba_api.stats.library.eventmsgtype import EventMsgType
 
 # regex patterns for all playbyplay and playbyplayv2 HOMEDESCRIPTION & VISITORDESCRIPTION fields 
 pattern_player_char = r'((?#char)(\. \w+)|(\-?\'?\w+))?'
 pattern_player_suffix = r'((?#suffix)([\s](Jr\.|III|II|IV)))?'
+pattern_referee = r'(?P<referee>.*)'
 pattern_team = r'(?P<team>\w+( \w+)?)'
 pattern_field_goal_type = r'(?P<field_goal_type>[\w+( |\-)]*)'
-pattern_free_throw_type = r'(?P<free_throw_type>(\w+ )?(\d of \d)|\w+)'
+pattern_free_throw_type = r'(?P<free_throw_type>(.* )?(\d of \d)|\w+)'
 pattern_player = r"(?P<player>\w+{player_char}{player_suffix})".format(player_char=pattern_player_char,player_suffix=pattern_player_suffix)
 pattern_points = r"\((?P<points>\d+) PTS\)"
 pattern_rebound_team = r'^{team} Rebound$'.format(team=pattern_team)
-pattern_turnover_team = r'^{team} Turnover: (?P<turnover_type>\w+\s?\w+) \(T#(?P<turnovers>\d.)\)$'.format(team=pattern_team)
+pattern_turnover_team = r'^{team} Turnover: (?P<turnover_type>.*) \(T#(?P<turnovers>\d+)\)$'.format(team=pattern_team)
 pattern_timeout = r'^{team} Timeout: ((?P<timeout_type>\w+)) \(\w+ (?P<full>\d+) \w+ (?P<short>\d+)\)$'.format(team=pattern_team)
 pattern_block = r"^{player} BLOCK \((?P<blocks>\d+) BLK\)$".format(player=pattern_player)                    
 pattern_ejection = r'^{player} Ejection:(?P<ejection_type>.*)$'.format(player=pattern_player)
 pattern_field_goal_made = r"^{player}\s{{1,2}}?((?P<distance>\d+)\' )?{field_goal_type} {points}( \({player_ast} (?P<assists>\d+) AST\))?$".format(
     player=pattern_player,player_ast=pattern_player.replace('player','player_ast'), field_goal_type=pattern_field_goal_type,points=pattern_points)
 pattern_field_goal_missed = r"^MISS {player}\s{{1,2}}?((?P<distance>\d+)\' )?{field_goal_type}$".format(player=pattern_player,field_goal_type=pattern_field_goal_type)
-pattern_foul = r"^{player} (?P<foul_type>.*)\s(?=(\(\w+(?P<personal>\d+)(\.\w+(?P<team>[\d|\w]+))?\) (\((?P<referee>\w.\w+)\)))$)".format(player=pattern_player)
+pattern_foul_player = r"^{player} (?P<foul_type>.*)\s(?=(\(\w+(?P<personal>\d+)(\.\w+(?P<team>[\d|\w]+))?\) (\({referee}\)))$)".format(player=pattern_player, referee=pattern_referee)
+pattern_foul_team = r"^{team} T.Foul \((?P<foul_type>.*) {player} \) (\({referee}\))".format(player=pattern_player, team=pattern_team, referee=pattern_referee)
 pattern_free_throw_made = r"^{player} Free Throw {free_throw_type} {points}$".format(player=pattern_player,free_throw_type=pattern_free_throw_type,points=pattern_points)
 pattern_free_throw_miss = r"^MISS {player} Free Throw {free_throw_type}$".format(player=pattern_player,free_throw_type=pattern_free_throw_type)
-pattern_jump_ball = r"^Jump Ball {player_home} vs. {player_away}: Tip to {player_tip}$".format(player_home=pattern_player.replace('player','player_home'),
+pattern_jump_ball = r"^Jump Ball {player_home} vs. {player_away}: Tip to( {player_tip})?$".format(player_home=pattern_player.replace('player','player_home'),
     player_away=pattern_player.replace('player','player_away'),
     player_tip=pattern_player.replace('player','player_tip'))
 pattern_rebound_player = r"^{player} REBOUND \(Off:(?P<offensive>\d+) Def:(?P<defensive>\d+)\)$".format(player=pattern_player)
@@ -27,7 +31,7 @@ pattern_steal = r"^{player}\s{{1,2}}?STEAL \((?P<steals>\d+) STL\)$".format(play
 pattern_substitution = r"^SUB: {player_in} FOR {player_out}$".format(
     player_in=pattern_player.replace('player','player_in'),player_out=pattern_player.replace('player','player_out'))
 pattern_turnover_player = r"^{player}\s{{1,2}}?(?P<turnover_type>[\w+?\-? ]*) (Turnover[ ]?)+ \(P(?P<personal>\d+)\.T(?P<team>\d+)\)$".format(player=pattern_player)
-pattern_violation = r"^{player} Violation:(?P<violation_type>\w+\s?\w+)\s\((?P<referee>\w.\w+)\)$".format(player=pattern_player)
+pattern_violation = r"^{player} Violation:(?P<violation_type>\w+\s?\w+)\s\({referee}\)$".format(player=pattern_player, referee=pattern_referee)
 
 # compiled regex for all playbyplay and playbyplayv2 HOMEDESCRIPTION & VISITORDESCRIPTION fields 
 re_block = re.compile(pattern_block)
@@ -36,7 +40,8 @@ re_field_goal_made = re.compile(pattern_field_goal_made)
 re_field_goal_missed = re.compile(pattern_field_goal_missed)
 re_free_throw_made = re.compile(pattern_free_throw_made)
 re_free_throw_miss = re.compile(pattern_free_throw_miss)
-re_foul = re.compile(pattern_foul)
+re_foul_player = re.compile(pattern_foul_player)
+re_foul_team = re.compile(pattern_foul_team)
 re_rebound_player = re.compile(pattern_rebound_player)
 re_rebound_team = re.compile(pattern_rebound_team)
 re_turnover_player = re.compile(pattern_turnover_player)
@@ -46,3 +51,16 @@ re_substitution = re.compile(pattern_substitution)
 re_timeout = re.compile(pattern_timeout)
 re_violation = re.compile(pattern_violation)
 re_jump_ball = re.compile(pattern_jump_ball)
+
+eventmsgtype_to_re = defaultdict(list, {
+    EventMsgType.EJECTION : [re_ejection],
+    EventMsgType.FIELD_GOAL_MADE : [re_field_goal_made],
+    EventMsgType.FIELD_GOAL_MISSED : [re_field_goal_missed, re_block],
+    EventMsgType.FREE_THROW : [re_free_throw_made, re_free_throw_miss],
+    EventMsgType.REBOUND : [re_rebound_player, re_rebound_team],
+    EventMsgType.TURNOVER : [re_turnover_player, re_steal, re_turnover_team],
+    EventMsgType.FOUL : [re_foul_player, re_foul_team],
+    EventMsgType.VIOLATION : [re_violation],
+    EventMsgType.SUBSTITUTION : [re_substitution],
+    EventMsgType.TIMEOUT : [re_timeout],
+    EventMsgType.JUMP_BALL : [re_jump_ball]})
---
+++ tests/stats/library/playbyplay/test_play_by_play_regex_integration.py
@@ -0,0 +1,59 @@
+import pytest
+import datetime
+import time
+import random
+from nba_api.stats.endpoints import Scoreboard
+from nba_api.stats.endpoints import PlayByPlay
+from nba_api.stats.endpoints import playbyplay
+from nba_api.stats.library.parameters import LeagueID
+from nba_api.stats.library.eventmsgtype import EventMsgType
+from nba_api.stats.library.playbyplayregex import eventmsgtype_to_re
+
+
+
+def pytest_generate_tests(metafunc):
+    if 'game' in metafunc.fixturenames and 'play' in metafunc.fixturenames:
+        metafunc.parametrize( "game, play", [(game_id, play_by_play) for game_id in get_game_ids() for play_by_play in get_play_by_play(game_id)])
+
+def get_game_ids():
+    gamefinder = Scoreboard(league_id=LeagueID.nba,
+                                day_offset=-1,
+                                game_date=datetime.datetime.now()) 
+
+    games_dict = gamefinder.get_normalized_dict()
+    games = []
+    for game in games_dict['GameHeader']:
+        games.append(game['GAME_ID'])
+
+    return games
+
+def get_play_by_play(game_id):
+    # Delay briefly to prevent throttling
+    time.sleep(.600)
+    return PlayByPlay(game_id).get_normalized_dict()['PlayByPlay']
+
+@pytest.fixture()
+def game(request):
+    return request.param
+
+@pytest.fixture()
+def play(request):
+    return request.param
+
+def test_play(game, play):
+    for count in range(0, 1):
+        dict_patterns = eventmsgtype_to_re[EventMsgType(play["EVENTMSGTYPE"])]
+        description = play["HOMEDESCRIPTION"] if count == 0 else play["VISITORDESCRIPTION"]
+
+        #Validate description
+        if description is not None:
+            match = False
+            for pattern in dict_patterns:
+                if pattern.match(description): match = True 
+
+            if match == False: 
+                msg = 'EVENTMSGTYPE {eventmsgtype}: [{description}]\n\tAttempted Patterns\n'.format(eventmsgtype=play["EVENTMSGTYPE"], description=description )
+                for pattern in dict_patterns:
+                    msg += '\t\t{pattern}\n'.format(pattern=pattern.pattern)
+                
+                assert False, msg
