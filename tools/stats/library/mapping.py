d020c78f01b04b6fa3d79aceb8fc5df382ca110d
Swar Patel <swar.m.patel@gmail.com>
2020-01-27 01:13:13 -0500

    Added new endpoint: TeamGameLogs

--- tools/stats/library/mapping.py
+++ tools/stats/library/mapping.py
@@ -151,6 +151,7 @@ endpoint_list = [
     'TeamFranchiseLeaders',
     'TeamFranchiseLeadersRank',
     'TeamGameLog',
+    'TeamGameLogs',
     'TeamGameSplitsStats',
     'TeamGameStreakFinder',
     'TeamGeneralSplitsStats',
