42b82028f75651ba9483201fdddddcb8d70acb69
Swar Patel <swar.m.patel@gmail.com>
2020-01-27 00:27:07 -0500

    Updating headers

--- nba_api/stats/library/http.py
+++ nba_api/stats/library/http.py
@@ -6,13 +6,17 @@ try:
     from nba_api.library.debug.debug import STATS_HEADERS
 except ImportError:
     STATS_HEADERS = {
-        'Host': 'stats.nba.com', 
-        'Connection': 'keep-alive', 
-        'Accept': 'application/json, text/plain, */*', 
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.97 Safari/537.36', 
-        'Referer': 'https://stats.nba.com/', 
-        'Accept-Encoding': 'gzip, deflate, br', 
-        'Accept-Language': 'en-US,en;q=0.9',
+        'Host': 'stats.nba.com',
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:72.0) Gecko/20100101 Firefox/72.0',
+        'Accept': 'application/json, text/plain, */*',
+        'Accept-Language': 'en-US,en;q=0.5',
+        'Accept-Encoding': 'gzip, deflate, br',
+        'x-nba-stats-origin': 'stats',
+        'x-nba-stats-token': 'true',
+        'Connection': 'keep-alive',
+        'Referer': 'https://stats.nba.com/',
+        'Pragma': 'no-cache',
+        'Cache-Control': 'no-cache',
     }
 
 
