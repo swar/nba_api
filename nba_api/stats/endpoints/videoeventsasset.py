82bb631d7ce9f94f9074f072722c744e8fb130d6
prateekjaipuria <59226272+prateekjaipuria@users.noreply.github.com>
2022-08-20 13:44:19 -0700

    added support for videoeventsasset endpoint which provides video clips information (#259)

---
+++ nba_api/stats/endpoints/videoeventsasset.py
@@ -0,0 +1,46 @@
+from nba_api.stats.endpoints._base import Endpoint
+from nba_api.stats.library.http import NBAStatsHTTP
+
+
+class VideoEventsAsset(Endpoint):
+    endpoint = 'videoeventsasset'
+    expected_data = {}
+
+    nba_response = None
+    data_sets = None
+    player_stats = None
+    team_stats = None
+    headers = None
+
+    def __init__(self,
+                 game_id,
+                 game_event_id=0,
+                 proxy=None,
+                 headers=None,
+                 timeout=30,
+                 get_request=True):
+        self.proxy = proxy
+        if headers is not None:
+            self.headers = headers
+        self.timeout = timeout
+        self.parameters = {
+                'GameID': game_id,
+                'GameEventID': game_event_id
+        }
+        if get_request:
+            self.get_request()
+    
+    def get_request(self):
+        self.nba_response = NBAStatsHTTP().send_api_request(
+            endpoint=self.endpoint,
+            parameters=self.parameters,
+            proxy=self.proxy,
+            headers=self.headers,
+            timeout=self.timeout,
+        )
+        self.load_response()
+        
+    def load_response(self):
+        data_sets = self.nba_response.get_data_sets()
+        self.data_sets = [Endpoint.DataSet(data=data_set) for data_set_name, data_set in data_sets.items()]
+
