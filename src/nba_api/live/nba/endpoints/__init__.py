4c0a9c3a1268dadc3eff7d0accdf490a57adb91b
Mohith Nagendra <mohith.nagendra@allergan.com>
2025-03-10 16:54:21 -0400

    chore(api): expose Odds endpoint in live NBA API package

--- src/nba_api/live/nba/endpoints/__init__.py
+++ src/nba_api/live/nba/endpoints/__init__.py
@@ -1,5 +1,6 @@
-__all__ = ["playbyplay", "boxscore", "scoreboard"]
+__all__ = ["playbyplay", "boxscore", "scoreboard", "odds"]
 
 from .playbyplay import PlayByPlay
 from .boxscore import BoxScore
 from .scoreboard import ScoreBoard
+from .odds import Odds
