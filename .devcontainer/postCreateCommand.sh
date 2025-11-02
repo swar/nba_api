6a344ac55d3c659d443df114fd9425cfbc5d6cf0
Randy Forbes <randy.forbes@gmail.com>
2025-11-02 16:44:00 +0000

    build: Modernized DevContainer files.

--- .devcontainer/postCreateCommand.sh
+++
@@ -1,7 +0,0 @@
-pip install --upgrade pip
-poetry install
-# Activate poetry environment
-source $(poetry env info --path)/bin/activate
-
-# Update PYTHONPATH to include the virtual environment's Python interpreter
-echo "export PYTHON_PATH=$(poetry env info --path)/bin/python" >> ~/.bashrc
