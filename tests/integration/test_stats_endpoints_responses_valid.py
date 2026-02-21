d2983e1b4756b246ce4f523a9642aec30e3debda
Swar <swar.m.patel@gmail.com>
2026-02-21 16:37:48 -0500

    Refactoring smoke tests

--- tests/integration/test_stats_endpoints_responses_valid.py
+++ tests/integration/test_stats_endpoints_responses_valid.py
@@ -1,12 +1,16 @@
+"""Broad live smoke sweep for stats endpoints.
+
+This module performs a coarse integration check that each deferred endpoint
+call succeeds and returns JSON that parses as valid. It is not a detailed
+behavior or contract test.
+"""
+
 import json
 import time
 
 import pytest
 
 from tests.integration.deferred_endpoints import DeferredEndpoint, deferred_endpoints
-
-# Once we run the test to call the endpoints, we'll cache the responses here.
-cached_eps = []
 
 
 # This method is passed to test_endpoints so console output will be generated with
@@ -17,29 +21,24 @@ def endpoint_id_func(param):
     return None
 
 
-# Run this test on each endpoint in deferred_endpoints.
-@pytest.mark.parametrize("deferred_endpoint", deferred_endpoints, ids=endpoint_id_func)
-def test_endpoints(deferred_endpoint):
-    """Test that each endpoint is callable.
+def call_endpoint_and_assert_valid_json(deferred_endpoint):
+    endpoint_name = deferred_endpoint.endpoint_class.__name__
 
-    This takes a very, very long time in total (10-20 minutes) because we don't
-    want to barrage the NBA site with requests."""
     # Delay briefly
     time.sleep(0.600)
-    # Call the API.
+
     try:
         response = deferred_endpoint()
     except json.decoder.JSONDecodeError:
-        endpoint_class = deferred_endpoint.endpoint_class
-        msg = f"Unable to decode response for {endpoint_class}"
-        pytest.fail(msg=msg)
-    # We want to hang onto all the responses so we don't need to re-retrieve
-    # them later.
-    cached_eps.append(response)
+        pytest.fail(msg=f"Unable to decode response for {endpoint_name}")
+
+    assert response.nba_response.valid_json(), (
+        f"Endpoint returned non-valid JSON: {endpoint_name}"
+    )
 
 
-def test_valid_json():
-    # Check that every called endpoint is valid json.
-    valid = [ep.nba_response.valid_json() for ep in cached_eps]
-    assert len(valid) == len(deferred_endpoints)
-    assert all(valid)
+# Run this test on each endpoint in deferred_endpoints.
+@pytest.mark.parametrize("deferred_endpoint", deferred_endpoints, ids=endpoint_id_func)
+def test_endpoints(deferred_endpoint):
+    """Smoke test each endpoint call and JSON validity with pacing."""
+    call_endpoint_and_assert_valid_json(deferred_endpoint)
